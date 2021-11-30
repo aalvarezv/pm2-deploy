89264d56230b00a6c05b1f81b79930866b17c0a3
aalvarez <alvarez.vargasgmail.com>
2021-11-30 00:36:25 -0300

    Se agrega parametro key para el uso de llave ssh deploy_rsa

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,11 +8,11 @@ module.exports = {
   deploy : {
     production : {
       user : 'root',
-      host : '143.198.108.177',
+      host : '143.198.230.143',
       ref  : 'origin/main',
       repo : 'git@github.com:aalvarezv/pm2-deploy.git',
       path : '/var/www/pm2-deploy',
-      //key : '/Users/alanalvarez/.ssh/id_rsa.pub',
+      key : '~/.ssh/deploy_rsa',
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 startOrRestart ecosystem.config.js --name pm2-deploy --env production',
       'pre-setup': ''
